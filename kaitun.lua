6449a879754a85ce6f417d5e93d1e99c162c149b
BinbinvoGamingVNN <122621965+BinbinvoGamingVNClone@users.noreply.github.com>
2024-12-14 17:35:25 +0700

    Create kaitun.lua

---
+++ kaitun.lua
@@ -0,0 +1,24 @@
+_G.Melee = { 
+    ['Godhuman'] = true
+}
+_G.Sword = { 
+    ['Saber'] = true,
+    ['Midnight Blade'] = true,
+    ['Shisui'] = true,
+    ['Saddi'] = true,
+    ['Wando'] = true,
+    ['Yama'] = true,
+    ['Koko'] = false,
+    ['Rengoku'] = true,
+    ['Canvander'] = true,
+    ['Buddy Sword'] = true,
+    ['Twin Hooks'] = true,
+    ['SpikeyTrident'] = true,
+    ['Hallow Scryte'] = true,
+    ['Dark Dagger'] = true,
+    ['Tushita'] = true,
+    ['True Triple Katana'] = true,
+    ['Cursed Dual Katana'] = true,
+    ['Shark Anchor'] = true
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KOBENFFGGHH/AttackHubV2/refs/heads/main/AttackHubKaitan.txt"))()
