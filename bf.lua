4684d64510ace03e491bb939ae3a637241451f6b
BinbinvoGamingVNN <122621965+BinbinvoGamingVNClone@users.noreply.github.com>
2024-12-14 17:35:38 +0700

    Create bf.lua

---
+++ bf.lua
@@ -0,0 +1,5 @@
+(getgenv()).Config = {
+ ["Fast Attack Mobile"] = true, -- ตีเร็วของมือถือ
+ ["Fast Attack PC"] = false -- ตีเร็วของคอม
+}
+loadstring(game:HttpGet('https://raw.githubusercontent.com/KOBENFFGGHH/Tots/refs/heads/main/AttackHub001.lua.txt'))()
